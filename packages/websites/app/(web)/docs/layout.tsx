ddea33dccd28ea2e3c0ecaf32c26c4c8ac46231e
Jiwei,Yuan <ji-weiyuan@outlook.com>
2025-12-31 19:29:36 +0000

    fix(web): extend docs sidebar border to connect with header/footer

--- packages/websites/app/(web)/docs/layout.tsx
+++ packages/websites/app/(web)/docs/layout.tsx
@@ -7,16 +7,17 @@ type DocsLayoutProps = {
 
 export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
-    <div className="flex gap-8 lg:gap-12">
+    // Use negative margin to extend beyond Container padding, connecting borders with Header/Footer
+    <div className="flex -mx-6 md:-mx-8 lg:-mx-10 -my-8 md:-my-10 lg:-my-12">
       {/* Left Sidebar - hidden on mobile */}
-      <aside className="hidden md:block w-56 shrink-0 border-r border-border pr-6">
-        <div className="sticky top-[calc(var(--header-height)+2rem)]">
+      <aside className="hidden md:block w-48 shrink-0 border-r border-border">
+        <div className="sticky top-[calc(var(--header-height))] py-8 md:py-10 lg:py-12 pl-6 md:pl-8 lg:pl-10 pr-4">
           <DocsSidebar />
         </div>
       </aside>
 
       {/* Main content area */}
-      <div className="flex-1 min-w-0">
+      <div className="flex-1 min-w-0 py-8 md:py-10 lg:py-12 px-6 md:px-8 lg:px-10">
         {children}
       </div>
     </div>
